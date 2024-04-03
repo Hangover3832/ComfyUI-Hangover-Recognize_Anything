9c47e2ed28b4f9ebb0fda2c2a168a24cac31276d
AlexL <github@leual.ch>
2024-04-03 09:37:24 +0200

    remove duplicate keywords

--- ho_recognize_anything.py
+++ ho_recognize_anything.py
@@ -23,6 +23,7 @@ from ram import inference_tag2text # as inference
 from ram import get_transform
 from folder_paths import models_dir
 from functools import partial
+import numpy
 
 class RecognizeAnything:
     MODEL_NAMES = ["ram_swin_large_14m.pth", "ram_plus_swin_large_14m.pth", "tag2text_swin_14m.pth"] # other/newer models can be added here
@@ -33,7 +34,7 @@ class RecognizeAnything:
         self.model = None
         self.modelname = ""
         self.image_size = 384
-        self.inference = None
+        # self.inference = None
 
     @classmethod
     def INPUT_TYPES(s):
@@ -42,7 +43,6 @@ class RecognizeAnything:
                 "image": ("IMAGE",),
                 "model": (s.MODEL_NAMES, {"default": s.MODEL_NAMES[1]},),
                 "device": (s.DEVICES, {"default": s.DEVICES[0]},),
-                #"中文": ("BOOLEAN", {"default": False},),
                 "spec_tag2text": ("STRING", {"default": ""},),
                 # "tag2text_threshold": ("INT", {"min": 0, "max": 100, "default": 68},),
             }
@@ -61,7 +61,7 @@ class RecognizeAnything:
 
         # delete some tags that may disturb captioning
         # 127: "quarter"; 2961: "back", 3351: "two"; 3265: "three"; 3338: "four"; 3355: "five"; 3359: "one"
-        delete_tag_index = [127,2961, 3351, 3265, 3338, 3355, 3359]
+        delete_tag_index = [127, 2961, 3351, 3265, 3338, 3355, 3359]
 
         if (self.model == None) or (self.modelname != model) or (device != self.device):
             del self.model
@@ -73,18 +73,13 @@ class RecognizeAnything:
 
             if model == RecognizeAnything.MODEL_NAMES[0]:
                 self.model = ram(pretrained=model_path, image_size=self.image_size, vit='swin_l')
-                self.inference = inference_ram
-                spec_tag2text = None
             elif model == RecognizeAnything.MODEL_NAMES[1]:
                 self.model = ram_plus(pretrained=model_path, image_size=self.image_size, vit='swin_l')
-                self.inference = inference_ram
-                spec_tag2text = None
             elif model == RecognizeAnything.MODEL_NAMES[2]:
                 self.model = tag2text(pretrained=model_path, image_size=self.image_size, vit='swin_b', delete_tag_index=delete_tag_index)
                 # self.model.threshold = tag2text_threshold  # threshold for tagging
-                self.inference = inference_tag2text
             else:
-                raise ValueError('No valid model was selected', model)
+                raise ValueError('RAM: No valid model was selected', model)
 
             self.model.eval()
             self.model = self.model.to(dev)
@@ -98,12 +93,14 @@ class RecognizeAnything:
             i = 255. * im.cpu().numpy()
             img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
             image = self.transform(img).unsqueeze(0).to(dev)
-            if  model == RecognizeAnything.MODEL_NAMES[2]:
-                res = self.inference(image, self.model, input_tag=spec_tag2text)
+            if model == RecognizeAnything.MODEL_NAMES[2]:
+                res = inference_tag2text(image, self.model, input_tag=spec_tag2text) 
             else:
-                res = self.inference(image, self.model)
-            tag0 += res[0].replace(' ', '').replace('|', ',') + '\n'
-            tag1 += res[1].replace(' ', '').replace('|', ',') + '\n' if (len(res) > 1) and res[1] else '\n'
+                res = inference_ram(image, self.model)
+
+            # remove duplicates and reformat the string:
+            tag0 =  ", ".join(list(numpy.unique(res[0].split(' | ')))) + '\n'
+            tag1 += ", ".join(list(numpy.unique(res[1].split(' | ')))) + '\n' if (len(res) > 1) and res[1] else '\n'
             tag2 += res[2] + '\n' if (len(res) > 2) and res[2] else '\n'
         
         return(tag0, tag1, tag2,)
